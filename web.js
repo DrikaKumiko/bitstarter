31b7dfbbb52b7ce3f78ad5c122fcc44ddb51df84
Adriana Kumiko Takahagui <k.takahagui@gmail.com>
2013-07-10 15:10:39 -0300

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buffer = new Buffer(50);	
+	buffer = fs.readFileSync('./index.html');
+	respondse.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
